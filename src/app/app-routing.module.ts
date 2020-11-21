859052e0dac98e0287dac2355ebc0e14e5449de3
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-20 22:31:08 -0300

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
+import { HeroInfosComponent } from './hero-infos/hero-infos.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeComponent },
+{ path: 'hero', component: HeroInfosComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
